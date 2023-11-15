119efde4bece5da43788b8ffed89bf5586ea4768
Pilar Fernández Nieto <rla10309@educastur.es>
2023-11-15 19:12:39 +0100

    Cambios en Usuario y Rol

--- src/main/java/com/dawes/modelo/RolVO.java
+++ src/main/java/com/dawes/modelo/RolVO.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -27,7 +28,7 @@ public class RolVO {
 	@Column(nullable = false, length = 20)
 	private String nombre;
 	
-	@OneToMany(mappedBy = "rol")
+	@OneToMany(mappedBy = "rol", fetch = FetchType.LAZY)
 	List<UsuarioVO> usuarios;
 	
 	public RolVO(String nombre) {
--- src/main/java/com/dawes/modelo/UsuarioVO.java
+++ src/main/java/com/dawes/modelo/UsuarioVO.java
@@ -4,9 +4,13 @@ package com.dawes.modelo;
 
 
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
-
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -26,7 +30,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Entity
 @Table(name="usuarios", uniqueConstraints = @UniqueConstraint(columnNames = {"dni"}))
-public class UsuarioVO {
+public class UsuarioVO implements UserDetails {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -60,6 +64,37 @@ public class UsuarioVO {
 	public String toString() {
 		return "UsuarioVO [idusuario=" + idusuario + ", username=" + username + ", apellidos=" + apellidos
 				+ ", password=" + password + ", dni=" + dni + ", email=" + email + ", rol=" + rol.getNombre() + "]";
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		List<GrantedAuthority> roles = new ArrayList<GrantedAuthority>();
+		roles.add(new SimpleGrantedAuthority(this.rol.toString()));
+		return roles;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
 	}
 
 		
